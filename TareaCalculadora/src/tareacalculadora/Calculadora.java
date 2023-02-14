ecc0c3d32b85e13095d3092f4fc33853e8c169b8
axelnerilucero <neriluceroaxel@gmail.com>
2023-02-13 19:36:30 -0600

    Cambios

--- TareaCalculadora/src/tareacalculadora/Calculadora.java
+++ TareaCalculadora/src/tareacalculadora/Calculadora.java
@@ -1,8 +1,57 @@
 package tareacalculadora;
 
 public class Calculadora {
+    
+    private String marca;
+    private String cientifica;
+    private String fuenteDeEnergia;
+    private String tipoDePatalla;
 
     public Calculadora() {
+    }
+    
+    public Calculadora(String marca, String cientifica, String fuenteDeEnergia, String tipoDePatalla) {
+        this.marca = marca;
+        this.cientifica = cientifica;
+        this.fuenteDeEnergia = fuenteDeEnergia;
+        this.tipoDePatalla = tipoDePatalla;
+    }
+    
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getCientifica() {
+        return cientifica;
+    }
+
+    public String getFuenteDeEnergia() {
+        return fuenteDeEnergia;
+    }
+
+    public String getTipoDePatalla() {
+        return tipoDePatalla;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setCientifica(String cientifica) {
+        this.cientifica = cientifica;
+    }
+
+    public void setFuenteDeEnergia(String fuenteDeEnergia) {
+        this.fuenteDeEnergia = fuenteDeEnergia;
+    }
+
+    public void setTipoDePatalla(String tipoDePatalla) {
+        this.tipoDePatalla = tipoDePatalla;
+    }
+    
+    @Override
+    public String toString(){
+        return "Calculadora " + cientifica + " marca: " + marca + " con " + fuenteDeEnergia + " y pantalla " + tipoDePatalla;
     }
     
     public int suma(int a, int b){
@@ -23,4 +72,5 @@ public class Calculadora {
         }
         return (float)a / (float)b;
     }
+
 }
--- TareaCalculadora/src/tareacalculadora/MainCalculadora.java
+++ TareaCalculadora/src/tareacalculadora/MainCalculadora.java
@@ -14,7 +14,9 @@ public class MainCalculadora {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Calculadora calculadora1 = new Calculadora();
+        Calculadora calculadora1 = new Calculadora("Casio", "cientifica", "Panel Solar", "LED");
+        String cal = calculadora1.toString();
+        System.out.println(cal);
         int resultadoSuma = calculadora1.suma(5, 4);
         System.out.println("El resultado de la suma es: " +resultadoSuma);
         int resultadoResta = calculadora1.resta(35, 44);
