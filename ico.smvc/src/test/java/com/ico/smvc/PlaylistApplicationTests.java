9e439871158e0d0e53a6e44616c16fe394435d30
axelnerilucero <neriluceroaxel@gmail.com>
2023-05-01 18:56:03 -0600

    Con HashMao

---
+++ ico.smvc/src/test/java/com/ico/smvc/PlaylistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ico.smvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
